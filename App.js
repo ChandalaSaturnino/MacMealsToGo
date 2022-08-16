0a282adbc2bcf09765f7561f02d5c257b58869df
ChandalaSaturnino <pepetron@gmail.com>
2022-08-16 13:31:25 -0500

    test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HUEVOS</Text>
+      <Text>Test</Text>
       <StatusBar style="auto" />
     </View>
   );
